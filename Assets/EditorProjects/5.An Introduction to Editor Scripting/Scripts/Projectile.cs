e58703db03b3d16d39a05bf8d6cf52ee1b3267c3
utkucnay <utkucnay@gmail.com>
2023-10-09 18:35:54 +0300

    Started Spline Tool

---
+++ Assets/EditorProjects/5.An Introduction to Editor Scripting/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [HideInInspector] new public Rigidbody rigidbody;
+    
+    public float damageRadius = 1;
+
+    void Reset()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+}
---
+++ Assets/EditorProjects/6.Creating a Spline Tool/Scripts/Editor/SplineComponentEditor.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SplineComponent))]
+public class SplineComponentEditor : Editor {
+
+    [DrawGizmo(GizmoType.NonSelected)]
+    static void DrawGizmosLoRes(SplineComponent spline, GizmoType gizmoType)
+    {
+        Gizmos.color = Color.white;
+        DrawGizmo(spline, 64);
+    }
+
+    [DrawGizmo(GizmoType.Selected)]
+    static void DrawGizmosHiRes(SplineComponent spline, GizmoType gizmoType)
+    {
+        Gizmos.color = Color.white;
+        DrawGizmo(spline, 1024);
+    }
+
+    static void DrawGizmo(SplineComponent spline, int stepCount)
+    {
+        if (spline.points.Count > 0)
+        {
+            var P = 0f;
+            var start = spline.GetNonUniformPoint(0);
+            var step = 1f / stepCount;
+            do
+            {
+                P += step;
+                var here = spline.GetNonUniformPoint(P);
+                Gizmos.DrawLine(start, here);
+                start = here;
+            } while (P + step <= 1);
+        }
+    }
+
+    public override void OnInspectorGUI() {
+        EditorGUILayout.HelpBox("Hold Shift and click to append and insert curve points. Backspace to delete points.", MessageType.Info);
+        var spline = target as SplineComponent;
+        GUILayout.BeginHorizontal();
+        var closed = GUILayout.Toggle(spline.closed, "Closed", "button");
+        if (spline.closed != closed)
+        {
+            spline.closed = closed;
+            spline.ResetIndex();
+        }
+        if (GUILayout.Button("Flatten Y Axis"))
+        {
+            Undo.RecordObject(target, "Flatten Y Axis");
+            //TODO: Flatten(spline.points);
+            spline.ResetIndex();
+        }
+        if (GUILayout.Button("Center around Origin"))
+        {
+            Undo.RecordObject(target, "Center around Origin");
+            //TODO: CenterAroundOrigin(spline.points);
+            spline.ResetIndex();
+        }
+        GUILayout.EndHorizontal();
+        
+    }
+}
